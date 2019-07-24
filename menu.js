1866fcd5fff0ba0b04ce80c2807dc87975a50afa
Ashutosh Jindal <Ash.Jindal@ext.ons.gov.uk>
2019-07-24 10:12:01 +0100

    Added more shortcuts

--- menu.js
+++ menu.js
@@ -1,22 +1,30 @@
 var replaceMenus = {
-  "Elastic Beanstalk":"EB",
-  "Route 53":"53",
-  "Data Pipeline":"Pipeline",
-  "Mobile Analytics": "MA",
-  "CloudFormation": "CF",
-  "Trusted Advisor": "TA",
-  "EC2 Container Service": "ECS",
-  "Elasticsearch Service": "ES",
-  "Elastic Container Service": "ECS",
-  "Simple Email Service": "SES"
-};
+    "Certificate Manager":"ACM",
+    "Elastic Beanstalk":"EB",
+    "Route 53":"53",
+    "Data Pipeline":"Pipeline",
+    "Mobile Analytics": "MA",
+    "CloudFormation": "CF",
+    "Trusted Advisor": "TA",
+    "EC2 Container Service": "ECS",
+    "Elasticsearch Service": "ES",
+    "Elastic Container Service": "ECS",
+   "Simple Notification Service": "SNS",
+   "Simple Queue Service": "SQS",
+   "CloudWatch": "CW",
+   "CloudTrail": "CT",
+   "Lambda": "ƛ",
+    "Simple Email Service": "SES"
+  };
 
 $(".nav-shortcut").each(function(){
-  var label = $($($(this)[0]).find("a")[0]).find(".service-label")[0];
-  var text = (label.innerText);
-  for (var m in replaceMenus) {
-    if (text == m){
-      label.innerText=replaceMenus[m];
+    var label = $($($(this)[0]).find("a")[0]).find(".service-label")[0];
+    var text = (label.innerText);
+    console.log("checking : " + text);
+    for (var m in replaceMenus) {
+      if (text == m){
+        console.log("Found: " + text);
+		label.innerText=replaceMenus[m];
+      }
     }
-  }
-});
+  });
