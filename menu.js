29bcbf5c58b7b86a9e95d9eb66e73864f55c5dc7
Ash Jindal <ajorpheus@users.noreply.github.com>
2021-12-14 23:45:09 +0000

    Updated for changes to the fav bar

--- menu.js
+++ menu.js
@@ -1,30 +1,35 @@
 var replaceMenus = {
-    "Certificate Manager":"ACM",
-    "Elastic Beanstalk":"EB",
-    "Route 53":"53",
-    "Data Pipeline":"Pipeline",
+    "Certificate Manager": "ACM",
+    "Elastic Beanstalk": "EB",
+    "Route 53": "53",
+    "Data Pipeline": "Pipeline",
     "Mobile Analytics": "MA",
     "CloudFormation": "CF",
     "Trusted Advisor": "TA",
     "EC2 Container Service": "ECS",
     "Elasticsearch Service": "ES",
     "Elastic Container Service": "ECS",
-   "Simple Notification Service": "SNS",
-   "Simple Queue Service": "SQS",
-   "CloudWatch": "CW",
-   "CloudTrail": "CT",
-   "Lambda": "ƛ",
-    "Simple Email Service": "SES"
-  };
+    "Simple Notification Service": "SNS",
+    "Simple Queue Service": "SQS",
+    "CloudWatch": "CW",
+    "CloudTrail": "CT",
+    "Lambda": "ƛ",
+    "Elastic Container Registry": "ECR",
+    "Certificate Manager": "Certs",
+    "Security Hub": "SecHub",
+    "GuardDuty": "GDuty",
+    "Simple Email Service": "SES",
+    "Trusted Advisor": "TrAdv"
+};
 
-$(".nav-shortcut").each(function(){
+($("ol[data-rbd-droppable-id*='global-nav-favorites-bar-list-edit-mode']").find('li')[0]).each(function () {
     var label = $($($(this)[0]).find("a")[0]).find(".service-label")[0];
     var text = (label.innerText);
     console.log("checking : " + text);
     for (var m in replaceMenus) {
-      if (text == m){
-        console.log("Found: " + text);
-		label.innerText=replaceMenus[m];
-      }
+        if (text == m) {
+            console.log("Found: " + text);
+            label.innerText = replaceMenus[m];
+        }
     }
-  });
+});
